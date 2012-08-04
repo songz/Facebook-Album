3bc0b5c747f7f48cb6ee25edc3a078eb9dd257e1
Song Zheng <coldangelfire311@gmail.com>
2012-08-03 21:59:39 -0700

    get sendImage

--- app.rb
+++ app.rb
@@ -15,3 +15,15 @@ post '/sendImage' do
     'Access-Control-Max-Age' => "1728000"
   return "success!"
 end
+
+
+get '/sendImage' do
+  #string_io = request.body
+  #p data_bytes = string_io.read
+  status 200
+  headers \
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS',
+    'Access-Control-Max-Age' => "1728000"
+  return "success!"
+end
